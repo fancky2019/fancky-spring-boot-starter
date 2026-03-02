51a9a871c57638db087dd2740abeb4453594f8ad
fancky2019 <709737588@qq.com>
2026-03-02 08:58:42 +0800

    打包生成注释

--- src/main/java/com/example/fanckyspringbootstarter/config/EnableFanckyStarter.java
+++ src/main/java/com/example/fanckyspringbootstarter/config/EnableFanckyStarter.java
@@ -6,9 +6,26 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
- * starter 两种方式：2比较灵活可以控制是否启动
- * 1、spring.factories 配置自动配置项
- * 2、@Import实现 ImportSelector 的selectImports方法，方法内添加自动配置类全路径
+ * starter 两种方式：2比较灵活，写在启动类上可以控制是否启动
+ * 1、spring.factories 配置自动配置项。引入jar 依赖即可自动装配
+ * 2、@EnableFanckyStarter写在启动类上可以控制是否启动：@Import实现 ImportSelector 的selectImports方法，方法内添加自动配置类全路径。
+ *
+ *
+ * package：该命令完成了项目编译、单元测试、打包功能三个过程。
+ * install：在 package 命令的前提下新增一个步骤，将新打好的包部署到本地 Maven 仓库。
+ * deploy：在 install 命令的前提下新增一个步骤，将新打的包部署到远端仓库（相当于本地和远端仓库同时部署一份）。
+ *
+ *
+ * 打包带注释
+ * 1、
+ *将项目编译并打包到本地仓库，并且打包后会保存注释：
+ * mvn clean source:jar install -Dmaven.test.skip=true
+ *
+ *如果单纯使用 idea 右上角的Lifecycle -> install 工具打包，打包后的 jar 是没有注释的
+ *
+ * 2、添加插件 maven-source-plugin，见pom
+ *
+ *
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
